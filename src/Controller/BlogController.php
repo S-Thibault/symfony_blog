e8b9e3d98da935ec73682b8d9dbd1fc2ae2fa77a
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-09-21 20:53:58 +0200

    sauvegarde 3

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -14,9 +14,7 @@ class BlogController extends AbstractController
     public function index()
     {
         $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $articles = $repo->findAll();
-
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
             'articles' => $articles
@@ -32,16 +30,24 @@ class BlogController extends AbstractController
     }
 
     /**
+    * @Route("/blog/new", name="blog_create")
+    */
+    public function create()
+    {
+      return $this-> render('blog/create.html.twig');
+    }
+
+    /**
      * @Route("/blog/{id}", name="blog_show")
      */
     public function show($id)
     {
         $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $article = $repo->find($id);
-
         return $this->render('blog/show.html.twig', ['article' => $article]);
     }
+
+
 
 
 }
