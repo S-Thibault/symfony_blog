13a40551417918ed3a48378a99a12d6f0bc44e83
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-09-23 15:37:08 +0200

    Sauvegarde 7

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,21 +5,56 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i <= 10; $i++) {
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("http://placehold.it/450x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($article);
+        // Création de 3 catégories fictives
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // Création de 4 à 6 articles fictifs par catéghorie
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>'; // Faker fait des paragraphes sous forme de tableaux donc il faut les 'éclater'
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+
+                $manager->persist($article);
+
+                // Création de 2 à 10 commentaires fictifs par article
+                for ($k = 1; $k <= mt_rand(2, 5); $k++) {
+                    $comment = new Comment();
+
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>'; // Comme pour les articles
+
+                    $now = new \DateTime();
+                    $days = $now->diff($article->getCreatedAt())->days; // On peut remplacer "$now" dirctement par "(new \DateTime())'"
+                    $minimum = '-' . $days . 'days';
+
+                    $comment->setAuthor($faker->firstName())
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimum)) // On peut remplacer "$minimum" dirctement par "'-' . $days . 'days'"
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
         }
-
         $manager->flush();
     }
 }
