e90c13cd649914eaa1313da40833a1495e7f381b
Philpof <philippe.perechodov@free.fr>
2020-09-24 17:01:43 +0200

    Sauvegarde 10

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -34,12 +34,28 @@ class SecurityController extends AbstractController
         $manager->persist($user);
         $manager->flush();
 
-        return $this-> redirectToRoute('home');
+        return $this-> redirectToRoute('security_login');
 
         }
 
         return $this->render('security/registration.html.twig', [
             'formRegistration' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout()
+    {
+
     }
 }
